e4d131125a40d2e15c786b3f6ff6e1364d741a9b
extav <143948204+extav@users.noreply.github.com>
2025-07-11 09:24:23 -0700

    Generated the map grid

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function populateMap() {
 
   const canv = document.createElement("canvas");
   canv.id = "mapcanvas";
-  canv.height = 300;
-  canv.width = 300;
+  canv.height = MAPSIZE;
+  canv.width = MAPSIZE;
   zone1.appendChild(canv);
 }
 
@@ -82,23 +82,92 @@ function inventoryButton() {
 
 // ----------- page setup --------- //
 
-function drawMap() {
-  const canv = document.getElementById("mapcanvas");
-  console.log(canv)
-  const context = canv.getContext("2d");
-  console.log(context)
-  context.fillStyle = "black";
-  // context.fillRect(10, 10, 50, 50);
-  context.beginPath();
-  context.arc(75, 75, 10, 0,  2 * Math.PI, 1);
-  context.fill();
+// ----------- Map stuff --------- //
+
+let map_position_x = -1.5;
+let map_position_y = -1.5;
+const MAPSIZE = 350;
+const MAPNODESIZE = 50;
+const mapnodes = new Set();
+
+class Mapnode {
+  constructor(type, mapx, mapy) {
+    this.type = type;
+    this.mapx = mapx;
+    this.mapy = mapy;
+  }
+
+  isAtLocation(x, y) {
+    // console.log("comparing " + this.mapx + " to " + x)
+    return this.mapx == x && this.mapy == y;
+  }
 }
 
+function drawMap() {
+  const canv = document.getElementById("mapcanvas");
+  console.log(canv);
+  const context = canv.getContext("2d");
+  console.log(context);
+  // context.arc(75, 75, 10, 0, 2 * Math.PI, 1);
+  const ntd = getNodesToDraw();
+
+  console.log(ntd);
+  console.log(mapnodes);
+  for (let mn of ntd.values()) {
+    if (mn.type == 1) {
+      context.fillStyle = "green";
+    } else if (mn.type == 2) {
+      context.fillStyle = "blue";
+    } else {
+      context.fillStyle = "grey";
+    }
+
+    const mn_pixelx = (mn.mapx - map_position_x) * 100;
+    const mn_pixely = (mn.mapy - map_position_y) * 100;
+
+    context.fillRect(mn_pixelx, mn_pixely, MAPNODESIZE, MAPNODESIZE);
+    console.log("filled rect");
+  }
+}
+
+function getNodesToDraw() {
+  const xvals = [];
+  const yvals = [];
+  for (let i = 1; i < 4; i++) {
+    xvals.push(Math.floor(map_position_x + i));
+    yvals.push(Math.floor(map_position_y + i));
+  }
+
+  const nodes = new Set();
+  for (let x of xvals) {
+    for (let y of yvals) {
+      let foundOne = false;
+      for (let mn of mapnodes) {
+        if (mn.isAtLocation(x, y)) {
+          nodes.add(mn);
+          foundOne = true;
+          console.log("found one!");
+        }
+      }
+      if (!foundOne) {
+        const newNode = new Mapnode(0, x, y);
+        mapnodes.add(newNode);
+        nodes.add(newNode);
+      }
+    }
+  }
+  return nodes;
+}
 // ----------- page setup --------- //
 window.onload = function () {
   document.getElementById("map-btn").onclick = mapButton;
   document.getElementById("inventory-btn").onclick = inventoryButton;
 
+  const m1 = new Mapnode(1, 0, 0);
+  mapnodes.add(m1);
+  const m2 = new Mapnode(2, 1, 0);
+  mapnodes.add(m2);
+
   populateMap();
-  drawMap()
+  drawMap();
 };
